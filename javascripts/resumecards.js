578845020a19fb964822f21cd4d910173bea6e3d
Elle Kasai <elle.kasai@gmail.com>
2015-01-12 22:56:20 -0800

    add event

--- javascripts/resumecards.js
+++ javascripts/resumecards.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
     $body.css('margin-top', 0);
   });
 
-  $themeMenu.on('click', function() {
+  $themeMenu.on('click', function(event) {
 
     $body.attr('class', '');
 
