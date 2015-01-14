65eed25505b2b8c5a743b1ad2246b34b2cb15d48
Elle Kasai <elle.kasai@gmail.com>
2015-01-13 21:25:09 -0800

    change nav position

--- javascripts/resumecards.js
+++ javascripts/resumecards.js
@@ -1,23 +1,7 @@
 $(document).ready(function () {
 
   var $body = $('body');
-  var $nav = $('nav');
-  var $header = $('header');
   var $themeMenu = $('.resume-dropdown-menu > li > a');
-
-  $nav.affix({
-    offset: {
-      top: $header.outerHeight()
-    }
-  });
-
-  $nav.on('affix.bs.affix', function (){
-    $body.css('margin-top', $nav.height() + parseInt($nav.css('margin-bottom')));
-  });
-
-  $nav.on('affix-top.bs.affix', function (){
-    $body.css('margin-top', 0);
-  });
 
   $themeMenu.on('click', function(event) {
 
